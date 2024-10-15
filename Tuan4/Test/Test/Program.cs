f57541ebd4c06a595a83e7bcbe2d17abcf0d62ca
Trần Quang Huy <huy.com2003@gmail.com>
2024-10-15 15:53:44 +0700

    Update

---
+++ Tuan4/Test/Test/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int[] numbers = { 3, 4, 5, 2, 5 };
+//query syntax
+IEnumerable<int> query1 = from num in numbers
+                          where num % 2 == 0
+                          orderby num
+                          select num;
+var query2 = numbers
+         .Where(n => n % 2 != 0)
+            .OrderBy(n => n);
+foreach (var item in query2)
+{
+    Console.WriteLine(item);
+}
