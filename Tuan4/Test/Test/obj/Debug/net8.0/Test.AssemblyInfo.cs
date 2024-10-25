96c2fda08514a94a1f2f775eda3a57faa98ad58d
Trần Quang Huy <huy.com2003@gmail.com>
2024-10-25 21:06:31 +0700

    Update

--- Tuan4/Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Tuan4/Test/Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477dc3a3f791c9b6d4d1b87bd3fe5b17a53e43f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5030b9fb380901816202425410a065be75d6d1e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
